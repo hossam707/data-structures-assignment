ca48ba39d690ee58fd30daa4551535f1d830f167
Hossam <hossam.mohammed@afaqy.com>
2021-06-16 15:47:33 +0200

    init repo

---
+++ src/main/java/net/datastructures/LookupTable.java
@@ -0,0 +1,117 @@
+package net.datastructures;
+
+import java.util.Comparator;
+
+public class LookupTable<K, V> implements SortedMap<K, V> {
+	
+	public LookupTable(){
+		// TODO Add necessary initialization
+	}
+	
+	/* 
+	 * @param comparator - on construction, set comparator
+	 */
+	public LookupTable(Comparator<K> comparator) {
+		// TODO Add necessary initialization
+	}
+	
+	/*
+	 * @param comparator - the comparator to set
+	 */
+	public void setComparator(Comparator<K> comparator) {
+		// TODO Add necessary initialization
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public V get(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V remove(K key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<K> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<V> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> firstEntry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> lastEntry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> ceilingEntry(K key) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> floorEntry(K key) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> lowerEntry(K key) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entry<K, V> higherEntry(K key) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Entry<K, V>> subMap(K fromKey, K toKey)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
